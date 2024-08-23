9e7d567880f021fee6e8cf5e23db1f93444062d0
Diya Ditto <diyaditto84@gmail.com>
2024-08-23 16:11:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-let sectionHomeEle = document.getElementById9("sectionHome");
-sectionHomeEle.textContent = 'home';
+const textAnimation = document.querySelector('.text-animation');
+const texts = ["Web Developer", "Web Designer"];
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
 
-let sectionLinkedInEle = document.getElementById("sectionLinkedIn");
-sectionLinkedInEle.textContent = "https://www.linkedin.com/in/diya-ditto-368446257";
+function type() {
+    if (count === texts.length) {
+        count = 0; // Reset the count to loop through the texts again
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+    
+    textAnimation.textContent = letter;
+
+    if (letter.length === currentText.length) {
+        setTimeout(() => {
+            erase(); // Call the erase function after the entire text has been typed out
+        }, 2000); // Pause before erasing
+    } else {
+        setTimeout(type, 100); // Continue typing
+    }
+}
+
+function erase() {
+    letter = currentText.slice(0, --index);
+    textAnimation.textContent = letter;
+
+    if (letter.length === 0) {
+        count++;
+        setTimeout(type, 500); // Start typing the next text after erasing
+    } else {
+        setTimeout(erase, 50); // Continue erasing
+    }
+}
+
+// Initial call to start the typing effect
+type();
